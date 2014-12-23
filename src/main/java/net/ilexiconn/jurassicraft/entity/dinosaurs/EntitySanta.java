3d050dc5eaa2a46f7ed81d540e3b170ded15e25a
TheLarsinator <TheLarsinatormodding@hotmail.com>
2014-12-23 15:18:30 +0100

    Working hard on animations

--- src/main/java/net/ilexiconn/jurassicraft/entity/dinosaurs/EntitySanta.java
+++ src/main/java/net/ilexiconn/jurassicraft/entity/dinosaurs/EntitySanta.java
@@ -2,169 +2,41 @@ package net.ilexiconn.jurassicraft.entity.dinosaurs;
 
 import net.ilexiconn.jurassicraft.ModItems;
 import net.ilexiconn.jurassicraft.Util;
-import net.ilexiconn.jurassicraft.entity.EntityJurassiCraftLandCoward;
+import net.ilexiconn.jurassicraft.ai.JurassiCraftEntityAIAvoidEntityIfNotTamed;
+import net.ilexiconn.jurassicraft.ai.JurassiCraftEntityAIEatDroppedFood;
+import net.ilexiconn.jurassicraft.ai.JurassiCraftEntityAIFollowFoodCoward;
+import net.ilexiconn.jurassicraft.ai.JurassiCraftEntityAIWander;
+import net.ilexiconn.jurassicraft.client.animation.AIHypsilophodonScratchHead;
+import net.ilexiconn.jurassicraft.entity.EntityJurassiCraftRidable;
 import net.ilexiconn.jurassicraft.entity.IDinosaur;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.EntityAIControlledByPlayer;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class EntitySanta extends EntityJurassiCraftLandCoward implements IDinosaur
+public class EntitySanta extends EntityJurassiCraftRidable implements IDinosaur
 {
-    public int courseChangeCooldown = 0;
-    public double waypointX;
-    public double waypointY;
-    public double waypointZ;
-    private Entity targetedEntity = null;
-    
-    
-    
-    private final EntityAIControlledByPlayer aiControlledByPlayer;
-    private boolean field_70279_a;
-    private double field_70276_b;
-    
-    /**
-     * Returns the Y offset from the entity's position for any entity riding this one.
-     */
-    public double getMountedYOffset()
-    {
-        return (double)this.height;
-    }
-
-    public void updateRiderPosition()
-    {
-        if (this.riddenByEntity != null)
-        {
-            this.riddenByEntity.setPosition(this.posX, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset(), this.posZ);
-        }
-    }
-
-    /**
-     * Called when a player interacts with a mob. e.g. gets milk from a cow, gets into the saddle on a pig.
-     */
-    public boolean interact(EntityPlayer var1)
-    {
-        ItemStack var2 = var1.getCurrentEquippedItem();
-
-        if (this.riddenByEntity != null && this.riddenByEntity instanceof EntityPlayer && this.riddenByEntity != var1)
-        {
-            return true;
-        }
-        else
-        {
-            if (!this.worldObj.isRemote)
-            {
-                var1.mountEntity(this);
-            	var1.addChatMessage(new ChatComponentText("You are now riding Santa's sleigh!"));
-
-            }
-
-            return true;
-        }
-    }
 
     public EntitySanta(World world)
     {
         super(world, (byte) Util.classToId(EntitySanta.class));
         this.getNavigator().setAvoidsWater(true);
-        this.isImmuneToFire = false;
-        this.tasks.addTask(8, this.aiControlledByPlayer = new EntityAIControlledByPlayer(this, 16.0F));
-        this.field_70279_a = true;
-        this.field_70276_b = 0.07D;
-        this.height = 1.2F;
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(2, this.aiSit);
+    	this.tasks.addTask(2, new AIHypsilophodonScratchHead(this));
+        this.tasks.addTask(3, new EntityAIAvoidEntity(this, EntityHerrerasaur.class, 16.0F, 1.0D * this.getCreatureSpeed(), 1.2D * this.getCreatureSpeed()));
+        this.tasks.addTask(4, new JurassiCraftEntityAIFollowFoodCoward(this, 1.1D * this.getCreatureSpeed()));
+        this.tasks.addTask(4, new JurassiCraftEntityAIEatDroppedFood(this, 16.0D));
+        this.tasks.addTask(5, new JurassiCraftEntityAIWander(this, 0.8D * this.getCreatureSpeed()));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(6, new EntityAILookIdle(this));
+
         this.setCreatureExperiencePoints(800);
     }
 
-    /**
-     * returns true if all the conditions for steering the entity are met. For pigs, this is true if it is being ridden
-     * by a player and the player is holding a carrot-on-a-stick
-     */
-    public boolean canBeSteered()
-    {
-        ItemStack var1 = ((EntityPlayer)this.riddenByEntity).getHeldItem();
-        return var1 != null && var1.getItem() == Items.carrot_on_a_stick;
-    }
-
-    protected void entityInit()
-    {
-        super.entityInit();
-        this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
-    }
-    
-    /**
-     * Called to update the entity's position/logic.
-     */
-    public void onUpdate()
-    {
-        super.onUpdate();
-        byte var1 = this.dataWatcher.getWatchableObjectByte(16);
-    }
-
-    protected void updateEntityActionState()
-    {
-
-        this.despawnEntity();
-        double var1 = this.waypointX - this.posX;
-        double var3 = this.waypointY - this.posY;
-        double var5 = this.waypointZ - this.posZ;
-        double var7 = var1 * var1 + var3 * var3 + var5 * var5;
-
-        if (var7 < 1.0D || var7 > 3600.0D)
-        {
-            this.waypointX = this.posX + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-            this.waypointY = this.posY + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-            this.waypointZ = this.posZ + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-        }
-
-        if (this.courseChangeCooldown-- <= 0)
-        {
-            this.courseChangeCooldown += this.rand.nextInt(5) + 2;
-            var7 = (double)MathHelper.sqrt_double(var7);
-
-            if (this.isCourseTraversable(this.waypointX, this.waypointY, this.waypointZ, var7))
-            {
-                this.motionX += var1 / var7 * 0.1D;
-                this.motionY += var3 / var7 * 0.1D;
-                this.motionZ += var5 / var7 * 0.1D;
-            }
-            else
-            {
-                this.waypointX = this.posX;
-                this.waypointY = this.posY;
-                this.waypointZ = this.posZ;
-            }
-        }
-    }
-    /**
-     * True if the Santa has an unobstructed line of travel to the waypoint.
-     */
-    private boolean isCourseTraversable(double par1, double par3, double par5, double par7)
-    {
-        double var9 = (this.waypointX - this.posX) / par7;
-        double var11 = (this.waypointY - this.posY) / par7;
-        double var13 = (this.waypointZ - this.posZ) / par7;
-        AxisAlignedBB var15 = this.boundingBox.copy();
-
-        for (int var16 = 1; (double)var16 < par7; ++var16)
-        {
-            var15.offset(var9, var11, var13);
-
-            if (!this.worldObj.getCollidingBoundingBoxes(this, var15).isEmpty())
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    
     @Override
     public int getTalkInterval()
     {
@@ -205,21 +77,15 @@ public class EntitySanta extends EntityJurassiCraftLandCoward implements IDinosa
 	@Override
 	protected void dropFewItems(boolean recentlyBeenHit, int enchantBonus) 
 	{
-		
+		float developmentFraction = this.getGrowthStage() / 120.0F;
+		int count = Math.round(1 + (2.0F * developmentFraction) + this.rand.nextInt(1 + (int) (2.0F * developmentFraction)) + this.rand.nextInt(1 + enchantBonus));
+		if (this.isBurning()) 
+		{
+			this.dropItem(ModItems.dinoSteak, count);
+		} 
+		else 
+		{
+			this.dropItem(Util.getMeat(Util.getCreatureFromId(this.getCreatureID())), count);
+		}
 	}
-	
-    /**
-     * Checks if the entity's current position is a valid location to spawn this entity.
-     */
-    public boolean getCanSpawnHere()
-    {
-        return this.rand.nextInt(20) == 0 && super.getCanSpawnHere();
-    }
-    /**
-     * Return the AI task for player control.
-     */
-    public EntityAIControlledByPlayer getAIControlledByPlayer()
-    {
-        return this.aiControlledByPlayer;
-    }
 }
