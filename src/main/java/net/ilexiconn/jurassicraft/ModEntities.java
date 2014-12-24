3276456d1a6946fc43423033880792f32442e33a
gegy1000 <gegy1000@gmail.com>
2014-12-24 08:23:02 +0200

    fix anky

--- src/main/java/net/ilexiconn/jurassicraft/ModEntities.java
+++ src/main/java/net/ilexiconn/jurassicraft/ModEntities.java
@@ -8,6 +8,8 @@ public class ModEntities implements IContentHandler
 {
     public void init()
     {
-        EntityRegistry.registerModEntity(EntityDinoEgg.class, "dino_egg", EntityRegistry.findGlobalUniqueEntityId(), JurassiCraft.instance, 64, 1, true);
+        int findGlobalUniqueEntityId = EntityRegistry.findGlobalUniqueEntityId();
+		EntityRegistry.registerModEntity(EntityDinoEgg.class, "dino_egg", findGlobalUniqueEntityId, JurassiCraft.instance, 64, 1, true);
+		EntityRegistry.registerGlobalEntityID(EntityDinoEgg.class, "dino_egg", findGlobalUniqueEntityId);
     }
 }
