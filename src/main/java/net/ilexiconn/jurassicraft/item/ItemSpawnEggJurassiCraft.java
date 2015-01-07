dc564fba140a8cfd77e42172611af57e587dfa28
iLexiconn <ilexiconn@gmail.com>
2015-01-07 19:25:42 +0100

    Fixed entity spawn thing

--- src/main/java/net/ilexiconn/jurassicraft/item/ItemSpawnEggJurassiCraft.java
+++ src/main/java/net/ilexiconn/jurassicraft/item/ItemSpawnEggJurassiCraft.java
@@ -10,6 +10,8 @@ import net.ilexiconn.jurassicraft.entity.EntityJurassiCraftCreature;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -102,32 +104,39 @@ public class ItemSpawnEggJurassiCraft extends Item {
 	@SideOnly(Side.CLIENT)
 	public void getSubItems(Item item, CreativeTabs tab, List list) {
 		for (Entry<Class<?>, Creature> creature : CreatureManager.getCreatures().entrySet()) {
-			if (!creature.getValue().getCreatureName().equals("santa")) list.add(new ItemStack(item, 1, creature.getValue().getCreatureID()));
+			if (creature.getValue().getCreatureName().equals("santa")) list.add(new ItemStack(item, 1, creature.getValue().getCreatureID()));
 		}
 	}
 
 	@Override
 	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int hitX, float hitY, float hitZ, float metadata) {
-		Block block = world.getBlock(x, y, z);
-		x += Facing.offsetsXForSide[hitX];
-		y += Facing.offsetsYForSide[hitX];
-		z += Facing.offsetsZForSide[hitX];
-		double yTranslation = 0.0D;
-		if (hitX == 1 && block.getRenderType() == 11) {
-			yTranslation = 0.5D;
-		}
-		EntityJurassiCraftCreature creature = spawnCreature(world, player, itemStack, (double) x + 0.5D, (double) y + yTranslation, (double) z + 0.5D);
-		if (creature != null) {
-			if (itemStack.hasDisplayName()) {
-				creature.setCustomNameTag(itemStack.getDisplayName());
+		if (world.isRemote) {
+			return true;
+		} else {
+			Block block = world.getBlock(x, y, z);
+			x += Facing.offsetsXForSide[hitX];
+			y += Facing.offsetsYForSide[hitX];
+			z += Facing.offsetsZForSide[hitX];
+			double yTranslation = 0.0D;
+			if (hitX == 1 && block.getRenderType() == 11) {
+				yTranslation = 0.5D;
 			}
-			if (!player.capabilities.isCreativeMode) {
-				itemStack.stackSize--;
+			EntityJurassiCraftCreature creature = (EntityJurassiCraftCreature) spawnCreature(world, player, itemStack, (double) x + 0.5D, (double) y + yTranslation, (double) z + 0.5D);
+			if (creature != null) {
+				if (creature instanceof EntityLivingBase && itemStack.hasDisplayName()) {
+					((EntityLiving) creature).setCustomNameTag(itemStack.getDisplayName());
+				}
+				if (!player.capabilities.isCreativeMode) {
+					itemStack.stackSize--;
+					if (itemStack.stackSize <= 0) {
+						itemStack = (ItemStack) null;
+					}
+				}
+				world.spawnEntityInWorld(creature);
+				creature.playLivingSound();
 			}
-			world.spawnEntityInWorld(creature);
-			creature.playLivingSound();
+			return true;
 		}
-		return true;
 	}
 
 	public static EntityJurassiCraftCreature spawnCreature(World world, EntityPlayer player, ItemStack egg, double x, double y, double z) {
@@ -167,6 +176,6 @@ public class ItemSpawnEggJurassiCraft extends Item {
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return (EntityJurassiCraftCreature) null;
 	}
 }
