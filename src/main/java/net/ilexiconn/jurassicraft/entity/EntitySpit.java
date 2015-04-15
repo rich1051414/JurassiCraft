74047a8564a22d814a89d46744f13d1935b26e47
gegy1000 <gegy1000@gmail.com>
2015-04-15 16:14:57 +0200

    Fix spit crash

--- src/main/java/net/ilexiconn/jurassicraft/entity/EntitySpit.java
+++ src/main/java/net/ilexiconn/jurassicraft/entity/EntitySpit.java
@@ -14,8 +14,6 @@ import net.minecraft.world.World;
  */
 public class EntitySpit extends EntityThrowable
 {
-    EntityDilophosaurus spitter;
-    
     public EntitySpit(World world)
     {
         super(world);
@@ -24,7 +22,6 @@ public class EntitySpit extends EntityThrowable
     public EntitySpit(World world, EntityLivingBase entity)
     {
         super(world, entity);
-        spitter = (EntityDilophosaurus) entity;
     }
     
     public EntitySpit(World p_i1775_1_, double p_i1775_2_, double p_i1775_4_, double p_i1775_6_)
@@ -32,16 +29,23 @@ public class EntitySpit extends EntityThrowable
         super(p_i1775_1_, p_i1775_2_, p_i1775_4_, p_i1775_6_);
     }
     
-    protected void onImpact(MovingObjectPosition p_70184_1_)
+    protected void onImpact(MovingObjectPosition mop)
     {
-        if (p_70184_1_.entityHit != null && p_70184_1_.entityHit instanceof EntityLivingBase && p_70184_1_.entityHit != spitter && (p_70184_1_.entityHit == spitter.getAttackTarget() || !(p_70184_1_.entityHit instanceof EntityDilophosaurus)))
+        EntityLivingBase thrower = getThrower();
+        
+        if(thrower instanceof EntityDilophosaurus)
         {
-            byte b0 = 1;
+            EntityDilophosaurus spitter = (EntityDilophosaurus) thrower;
             
-            p_70184_1_.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float) b0);
-            ((EntityLivingBase) p_70184_1_.entityHit).addPotionEffect(new PotionEffect(Potion.blindness.id, 300, 1, false));
-            ((EntityLivingBase) p_70184_1_.entityHit).addPotionEffect(new PotionEffect(Potion.poison.id, 300, 1, false));
-            
+            if (mop.entityHit != null && mop.entityHit instanceof EntityLivingBase && mop.entityHit != spitter && (mop.entityHit == spitter.getAttackTarget() || !(mop.entityHit instanceof EntityDilophosaurus)))
+            {
+                EntityLivingBase entityHit = (EntityLivingBase) mop.entityHit;
+                
+                entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 1.0f);
+                entityHit.addPotionEffect(new PotionEffect(Potion.blindness.id, 300, 1, false));
+                entityHit.addPotionEffect(new PotionEffect(Potion.poison.id, 300, 1, false));
+                
+            }
         }
         //        this.
         
